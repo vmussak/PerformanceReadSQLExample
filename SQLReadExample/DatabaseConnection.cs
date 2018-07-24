bb6028aacd569f8360859d0140b1c27067bf6da9
Vinicius Mussak <vinicius.mussak@outlook.com>
2018-07-23 21:47:26 -0300

    Adicionar arquivos de projeto.

---
+++ SQLReadExample/DatabaseConnection.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLReadExample
+{
+    public class DatabaseConnection
+    {
+        private readonly SqlConnection _connection;
+
+        public DatabaseConnection()
+        {
+            var connectionString = @"server=DESKTOP-S8K8NS4\SQLEXPRESS;Database=GeradorEtiquetas;Integrated Security=SSPI;";
+            _connection = new SqlConnection(connectionString);
+        }
+
+        public void Insert()
+        {
+            _connection.Open();
+
+            for (int i = 0; i < 10000000; i++)
+            {
+                new SqlCommand($"INSERT INTO Cliente (Nome, DataNascimento, ClienteEspecial, NomeDaMae, QuantidadeFilhos) VALUES ('Cliente {i}', GETDATE(), {i % 2}, 'Mae {i}', {i.ToString().Substring(0, 1)})", _connection).ExecuteNonQuery();
+            }
+        }
+
+        public IEnumerable<Cliente> BuscarClientes()
+        {
+            var query = "SELECT * FROM Cliente";
+            var lstCliente = new List<Cliente>();
+
+            _connection.Open();
+            var cmd = new SqlCommand(query, _connection);
+            using (var reader = cmd.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    var time = new Stopwatch();
+                    time.Start();
+
+                    while (reader.Read())
+                        lstCliente.Add(new Cliente
+                        {
+                            Id = (int)reader["Id"],
+                            Nome = reader["Nome"].ToString(),
+                            DataNascimento = (DateTime)reader["DataNascimento"],
+                            ClienteEspecial = (bool)reader["ClienteEspecial"],
+                            NomeDaMae = reader["NomeDaMae"].ToString(),
+                            QuantidadeFilhos = (byte)reader["QuantidadeFilhos"]
+                        });
+
+                    time.Stop();
+                    Console.WriteLine("Tempo buscando pelo nome da coluna: {0}", time.Elapsed);
+                }
+            }
+
+            _connection.Close();
+
+            return lstCliente;
+        }
+
+        public IEnumerable<Cliente> BuscarClientes2()
+        {
+            var query = "SELECT * FROM Cliente";
+            var lstCliente = new List<Cliente>();
+
+            _connection.Open();
+            var cmd = new SqlCommand(query, _connection);
+            using (var reader = cmd.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    var time = new Stopwatch();
+                    time.Start();
+
+                    int id = reader.GetOrdinal("Id"),
+                        nome = reader.GetOrdinal("Nome"),
+                        dataNascimento = reader.GetOrdinal("DataNascimento"),
+                        clienteEspecial = reader.GetOrdinal("ClienteEspecial"),
+                        nomeDaMae = reader.GetOrdinal("NomeDaMae"),
+                        quantidadeFilhos = reader.GetOrdinal("QuantidadeFilhos");
+
+                    while (reader.Read())
+                        lstCliente.Add(new Cliente
+                        {
+                            Id = reader.GetInt32(id),
+                            Nome = reader.GetString(nome),
+                            DataNascimento = reader.GetDateTime(dataNascimento),
+                            ClienteEspecial = reader.GetBoolean(clienteEspecial),
+                            NomeDaMae = reader.GetString(nomeDaMae),
+                            QuantidadeFilhos = reader.GetByte(quantidadeFilhos)
+                        });
+
+                    time.Stop();
+                    Console.WriteLine("Tempo utilizando o GetOrdinal: {0}", time.Elapsed);
+                }
+            }
+
+            _connection.Close();
+
+            return lstCliente;
+        }
+
+    }
+
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public bool ClienteEspecial { get; set; }
+        public string NomeDaMae { get; set; }
+        public byte QuantidadeFilhos { get; set; }
+    }
+}
---
+++ SQLReadExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLReadExample
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            var db = new DatabaseConnection();
+
+            var clientes1 = db.BuscarClientes();
+
+            var clientes2 = db.BuscarClientes2();
+
+            Console.ReadKey();
+        }
+    }
+}
